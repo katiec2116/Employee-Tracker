78740a49c47cad3a4afe33fca47bba7bdf1b1202
katiec <katiecampbell21@outlook.com>
2020-10-11 09:38:40 -0400

    create connection and require packages

---
+++ db/connection.js
@@ -0,0 +1,7 @@
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "employees" 
+})
---
+++ index.js
@@ -0,0 +1,17 @@
+// require statements
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+
+// function init()
+
+function init (){
+    // load prompts
+    loadPrompts();
+}
+
+function loadPrompts(){
+
+
+
+}
