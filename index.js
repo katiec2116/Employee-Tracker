c941c6f06ed2b0f4b52c44587b0c23b59dfac323
katiec <katiecampbell21@outlook.com>
2020-10-14 20:12:03 -0400

    added chalk npm, styled main menu and ascii art

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // require statements
 const inquirer = require("inquirer");
 const connection = require("./db/connection.js");
-const DB = require("./db/dbFunctions.js")
-const logo = require("asciiart-logo")
+const DB = require("./db/dbFunctions.js");
+const logo = require("asciiart-logo");
+const chalk = require('chalk');
 
 const init = () => {
-    const logoText = logo({ name: "Employee Manager"}).render();
+    const logoText = logo({
+        name: "Employee Manager",
+        padding: 2,
+        margin: 3,
+        logoColor: 'bold-cyan',
+        borderColor: 'bold-magenta'
+    }).render();
     console.log(logoText)
     setTimeout(() => {
         console.log("\n")
@@ -13,13 +20,17 @@ const init = () => {
     }, 1000)
 }
 
-function loadPrompts(){
+
+
+
+
+function loadPrompts() {
     inquirer.prompt([
         {
             type: "list",
             name: "action",
             message: "What would you like to do?",
-            choices: ["View All Departments", "View All Employees", "View All Roles", "Add New Department", "Add New Employee", "Add New Role", "Update Employee Role", "Update Employees Manager", "View Employees by Manager", "Delete Department", "Delete Employee", "Delete Role", "Exit"]
+            choices: [chalk.cyan("View All Departments"), chalk.cyan("View All Roles"), chalk.cyan("View All Employees"), chalk.magenta("Add New Department"), chalk.magenta("Add New Role"), chalk.magenta("Add New Employee"), chalk.yellow("Update Employee Role"), chalk.yellow("Update Employees Manager"),  chalk.blue("Delete Department"), chalk.blue("Delete Role"), chalk.blue("Delete Employee"), chalk.white("View Employees by Manager"), chalk.white("View Departments Utilized Budget"), chalk.red("Exit")]
         }])
 
         .then(answer => {
@@ -28,20 +39,20 @@ function loadPrompts(){
                 case "View All Departments":
                     return DB.viewDepartments();
 
-                case "View All Employees":
-                    return DB.viewEmployees();
-
                 case "View All Roles":
                     return DB.viewRoles();
+
+                case "View All Employees":
+                    return DB.viewEmployees();
 
                 case "Add New Department":
                     return DB.addDepartment();
 
-                case "Add New Employee":
-                    return DB.addEmployee();
-
                 case "Add New Role":
                     return DB.addRole();
+
+                case "Add New Employee":
+                    return DB.addEmployee();
 
                 case "Update Employee Role":
                     return DB.updateEmployee();
@@ -55,11 +66,14 @@ function loadPrompts(){
                 case "Delete Department":
                     return DB.deleteDepartment();
 
+                case "Delete Role":
+                    return DB.deleteRole();
+
                 case "Delete Employee":
                     return DB.deleteEmployee();
 
-                case "Delete Role":
-                    return DB.deleteRole();
+                case "View Departments Utilized Budget":
+                    return DB.departmentBudget();
 
                 case "Exit":
                     connection.end();
